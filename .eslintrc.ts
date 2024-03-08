b697308c7d411a1c271f2085040f21161ae92622
nimod7890 <nimod1234@g.skku.edu>
2024-03-08 22:54:56 +0900

    chore: set eslint, prettier, tsconfig

--- .eslintrc.ts
+++ .eslintrc.ts
@@ -3,15 +3,16 @@ module.exports = {
   env: {
     browser: true,
     node: true,
-    es6: true,
+    es2020: true,
   },
-  parser: "@typescript-eslint/parser", // Specifies the ESLint parser
+  parser: "@typescript-eslint/parser",
   parserOptions: {
     ecmaVersion: "latest", // Allows for the parsing of modern ECMAScript features
     sourceType: "module", // Allows for the use of imports
     ecmaFeatures: {
       jsx: true, // Allows for the parsing of JSX
     },
+    project: "./tsconfig.json",
   },
   settings: {
     react: {
@@ -21,25 +22,68 @@ module.exports = {
       typescript: {},
     },
   },
-  // React 소스 일 경우
   extends: [
     "react-app",
-    "plugin:react/recommended", // Uses the recommended rules from @eslint-plugin-react
-    "plugin:@typescript-eslint/recommended", // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    "plugin:prettier/recommended", // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
+    "plugin:react/recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:prettier/recommended",
+    "eslint:recommended",
+    "plugin:react-hooks/recommended",
+    "prettier",
   ],
-  plugins: ["@typescript-eslint", "react", "react-hooks"],
+  ignorePatterns: ["dist"],
+  plugins: [
+    "no-relative-import-paths",
+    "react-refresh",
+    "@typescript-eslint",
+    "import",
+    "react",
+    "react-hooks",
+  ],
 
-  // Server 소스 일 경우
-  /*extends: [
-    'plugin:@typescript-eslint/recommended', // Uses the recommended rules from the @typescript-eslint/eslint-plugin
-    'plugin:prettier/recommended', // Enables eslint-plugin-prettier and eslint-config-prettier. This will display prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-  ],
-  plugins: ['@typescript-eslint',],*/
   rules: {
     "no-console": process.env.NODE_ENV === "prod" ? "warn" : "off",
     "no-debugger": process.env.NODE_ENV === "prod" ? "warn" : "off",
-    "import/extensions": ["off"],
-    // '@typescript-eslint/explicit-function-return-type': 'off',
+    "no-implicit-coercion": "error",
+    "no-warning-comments": [
+      "warn",
+      {
+        terms: ["TODO", "FIXME", "XXX", "BUG"],
+        location: "anywhere",
+      },
+    ],
+    indent: ["error", 2, { SwitchCase: 1 }],
+    "no-trailing-spaces": "error",
+    curly: "error",
+    "brace-style": "error",
+    "no-multi-spaces": "error",
+    "space-infix-ops": "error",
+    "space-unary-ops": "error",
+    "no-whitespace-before-property": "error",
+    "func-call-spacing": "error",
+    "space-before-blocks": "error",
+    "keyword-spacing": ["error", { before: true, after: true }],
+    "comma-spacing": ["error", { before: false, after: true }],
+    "comma-style": ["error", "last"],
+    "space-in-parens": ["error", "never"],
+    "block-spacing": "error",
+    "array-bracket-spacing": ["error", "never"],
+    "object-curly-spacing": ["error", "always"],
+    "key-spacing": ["error", { mode: "strict" }],
+    "arrow-spacing": ["error", { before: true, after: true }],
+    "react/react-in-jsx-scope": "off",
+    "@typescript-eslint/no-empty-function": "off",
+    "react-refresh/only-export-components": "off",
+    "@typescript-eslint/naming-convention": [
+      "error",
+      {
+        format: ["camelCase", "UPPER_CASE", "PascalCase"],
+        selector: "variable",
+        leadingUnderscore: "allow",
+      },
+      { format: ["camelCase", "PascalCase"], selector: "function" },
+      { format: ["PascalCase"], selector: "interface" },
+      { format: ["PascalCase"], selector: "typeAlias" },
+    ],
   },
 };
