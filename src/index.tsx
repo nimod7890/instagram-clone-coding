50672e6ecd98e631edc1528b50008a6b6c31c1e8
nimod7890 <nimod1234@g.skku.edu>
2024-03-09 14:27:16 +0900

    feat: sofia pro font 적용

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import "react-app-polyfill/stable";
 import ReactDOM from "react-dom";
 import App from "src/App";
+import "src/styles/fonts/index.css";
 
 ReactDOM.render(
   <StrictMode>
