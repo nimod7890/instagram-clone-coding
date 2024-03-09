3cd66ad1c986c6602ef19609eac25113fee0318c
nimod7890 <nimod1234@g.skku.edu>
2024-03-09 17:17:29 +0900

    improve: 비밀번호 값 없을 경우 비밀번호 표시 버튼 가리기

--- src/components/@common/PasswordInput.tsx
+++ src/components/@common/PasswordInput.tsx
@@ -1,3 +1,4 @@
+import { isEmpty } from "lodash";
 import { forwardRef, useState } from "react";
 import { Input } from "src/components/@common";
 import { InputProps } from "src/components/@common/Input";
@@ -8,10 +9,17 @@ const PasswordInput = forwardRef<HTMLInputElement, InputProps>((props, ref) => {
   const [showPassword, toggleShowPassword] = useState<boolean>(false);
 
   return (
-    <Input type={showPassword ? "text" : "password"} ref={ref} {...props}>
-      <StyledButton onClick={() => toggleShowPassword(!showPassword)}>
-        {showPassword ? "숨기기" : "비밀번호 표시"}
-      </StyledButton>
+    <Input
+      type={showPassword ? "text" : "password"}
+      ref={ref}
+      placeholder="비밀번호"
+      {...props}
+    >
+      {isEmpty(props.value) ? null : (
+        <StyledButton onClick={() => toggleShowPassword(!showPassword)}>
+          {showPassword ? "숨기기" : "비밀번호 표시"}
+        </StyledButton>
+      )}
     </Input>
   );
 });
