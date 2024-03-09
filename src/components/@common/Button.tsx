b42d3b2bc2bbb49a4968cc41f50334189cbae5d6
nimod7890 <nimod1234@g.skku.edu>
2024-03-09 16:59:48 +0900

    feat: create button component

---
+++ src/components/@common/Button.tsx
@@ -0,0 +1,73 @@
+import { KeyOfPalette, theme } from "src/styles";
+import styled, { css } from "styled-components";
+
+const Button = styled.button<{
+  backgroundColor?: KeyOfPalette;
+  width?: string | number;
+  variant?: keyof typeof buttonVariant;
+}>`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+
+  width: ${({ width }) => (width ? width : "100%")};
+  ${({ variant = "primary" }) => buttonVariant[variant]}
+
+  padding: 10px 0px;
+
+  cursor: pointer;
+
+  border: none;
+  border-radius: 30px;
+
+  ${theme.typography.body1Bold};
+
+  transition: 0.2s all;
+
+  :hover {
+    box-shadow: 1px 1px 1px ${theme.palette.gray100};
+  }
+
+  :active {
+    transform: scale(0.99);
+  }
+
+  :disabled {
+    cursor: default;
+    opacity: 0.5;
+    :active {
+      transform: none;
+    }
+    :hover {
+      box-shadow: none;
+    }
+  }
+`;
+
+export default Button;
+
+const buttonVariant = {
+  primary: css`
+    background-color: ${theme.palette.primary500};
+    color: ${theme.palette.white};
+    :disabled {
+      background-color: ${theme.palette.primary200};
+      opacity: 1;
+    }
+  `,
+
+  secondary: css`
+    background-color: ${theme.palette.white};
+    color: ${theme.palette.primary500};
+  `,
+
+  kakao: css`
+    background-color: ${theme.palette.kakaoYellow};
+    color: ${theme.palette.kakaoBlack};
+  `,
+
+  neutral: css`
+    color: ${theme.palette.white};
+    background-color: ${theme.palette.button};
+  `,
+};
