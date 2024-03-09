1f7052d41af2099fd348ec82f0af4379b2aef3ec
nimod7890 <nimod1234@g.skku.edu>
2024-03-09 17:31:50 +0900

    design: 한국어 폰트 임시 적용

--- src/styles/theme/typography.ts
+++ src/styles/theme/typography.ts
@@ -5,25 +5,25 @@ const typography = {
   body1Regular: css`
     font-size: 16;
     font-weight: 500;
-    font-family: "Sofia Pro";
+    font-family: "Sofia Pro", "Noto Sans";
   `,
   /** size 16; weight 600 */
   body1Bold: css`
     font-size: 16;
     font-weight: 600;
-    font-family: "Sofia Pro SemiBold";
+    font-family: "Sofia Pro SemiBold", "Noto Sans";
   `,
   /** size 14; weight 500 */
   body2Regular: css`
     font-size: 14;
     font-weight: 500;
-    font-family: "Sofia Pro";
+    font-family: "Sofia Pro", "Noto Sans";
   `,
   /** size 14; weight 600 */
   body2Bold: css`
     font-size: 14;
     font-weight: 600;
-    font-family: "Sofia Pro SemiBold";
+    font-family: "Sofia Pro SemiBold", "Noto Sans";
   `,
 } as const;
 
