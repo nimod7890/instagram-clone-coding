38bb043290852498b4eb43f44eb4dea460c223bc
nimod7890 <nimod1234@g.skku.edu>
2024-03-09 16:58:16 +0900

    refactor: remove css attributes from typography

--- src/components/@common/Typography.tsx
+++ src/components/@common/Typography.tsx
@@ -1,16 +1,13 @@
 import { KeyOfPalette, KeyOfTypography, theme } from "src/styles";
-import styled, { css } from "styled-components";
+import styled from "styled-components";
 
 const Typography = styled.p<{
   type?: KeyOfTypography;
   color?: KeyOfPalette;
-  css?: ReturnType<typeof css>;
 }>`
   ${({ type = "body1Regular" }) => theme.typography[type]};
   color: ${({ color = "black" }) => theme.palette[color]};
   line-height: 24px;
-
-  ${({ css }) => css}
 `;
 
 export default Typography;
