0d6d14680955e46bdfd44bb03e1a917941b0716b
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 14:40:30 +0900

    update: react 18

--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,11 @@
 import { StrictMode } from "react";
 import "react-app-polyfill/stable";
-import ReactDOM from "react-dom";
+import ReactDOM from "react-dom/client";
 import App from "src/App";
 import "src/styles/fonts/index.css";
 
-ReactDOM.render(
+ReactDOM.createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-  document.getElementById("root")
+  </StrictMode>
 );
