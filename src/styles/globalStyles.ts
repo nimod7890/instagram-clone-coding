df4276e3558a63fdd24d68ca6f56dbcc54ec5f83
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 21:35:39 +0900

    design: update global style

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -5,23 +5,77 @@ import { createGlobalStyle } from "styled-components";
 export const supportDeviceSize = 1080;
 
 export const GlobalStyle = createGlobalStyle`
-
-body {
+  body {
     font-family: "Sofia Pro", "Noto Sans", -apple-system, sans-serif, Roboto;
     line-height: 24px;
-    text-align: center;
-    
+
     margin: 0;
     padding: 0;
-    
+
     display: flex;
     flex-direction: column;
-    
+
     background-color: ${theme.palette.background};
     min-width: ${GlobalSize.WindowWidthMin};
     min-height: 100vh;
-    height:100vh;
+    height: 100vh;
 
     overflow: hidden;
-}
+  }
+
+  button {
+    min-width: max-content;
+    background: inherit;
+    border: none;
+    box-shadow: none;
+    border-radius: 0;
+    padding: 0;
+    overflow: visible;
+    cursor: pointer;
+    user-select: none;
+    -webkit-user-select: none;
+    -moz-user-select: none;
+    -ms-user-select: none;
+
+    transition: 0.2s all;
+
+    ${theme.typography.body1Bold};
+  }
+  button:hover{
+    transform: scale(1.02);
+  }
+  button:active {
+    transform: scale(0.99);
+  }
+
+  button:focus {
+    outline: none;
+  }
+  button:disabled {
+    cursor: default;
+    pointer-events: none;
+    :active {
+      transform: none;
+    }
+    :hover {
+      box-shadow: none;
+    }
+  }
+
+  input:focus {
+    outline: none;
+  }
+  
+  textarea:focus {
+    outline: none;
+  }
+  input,
+  textarea {
+    border: none;
+    margin: 0;
+
+    *:required {
+      background-color: ${theme.palette.white};
+    }
+  }
 `;
