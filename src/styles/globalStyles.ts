fa13fa3a7051371da26329746b6163a087a8f2b1
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 16:51:49 +0900

    design: update body css

--- src/styles/globalStyles.ts
+++ src/styles/globalStyles.ts
@@ -1,4 +1,5 @@
 import theme from "./theme/theme";
+import { GlobalSize } from "src/constants";
 import { createGlobalStyle } from "styled-components";
 
 export const supportDeviceSize = 1080;
@@ -7,11 +8,18 @@ export const GlobalStyle = createGlobalStyle`
 
 body {
     font-family: "Sofia Pro", "Noto Sans", -apple-system, sans-serif, Roboto;
-    background-color: ${theme.palette.background};
+    
     margin: 0;
     padding: 0;
-    height:100vh;
+    
     display: flex;
     flex-direction: column;
+    
+    background-color: ${theme.palette.background};
+    min-width: ${GlobalSize.WindowWidthMin};
+    min-height: 100vh;
+    height:100vh;
+
+    overflow: hidden;
 }
 `;
