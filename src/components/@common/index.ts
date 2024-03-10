aa9689c2ccf379faa576074975cab39a1d1dc9f5
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 22:27:18 +0900

    refactor: barrel export

--- src/components/@common/index.ts
+++ src/components/@common/index.ts
@@ -4,7 +4,11 @@ export { default as Typography } from "./Typography";
 export { default as Logo } from "./Logo";
 
 export { default as Button } from "./button/Button";
+export { default as IconButton } from "./button/IconButton";
+export { default as NavIconButton } from "./button/NavIconButton";
 
 export { default as Input } from "./input";
 export { default as PasswordInput } from "./input/PasswordInput";
 export { default as SearchInput } from "./input/SearchInput";
+
+export { default as Avatar } from "./Avatar";
