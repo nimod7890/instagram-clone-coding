44927c1759b2160176f4e92d683859c412abae6c
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 14:05:24 +0900

    design: add global styles

--- src/libraries/index.tsx
+++ src/libraries/index.tsx
@@ -2,11 +2,13 @@ import { PropsWithChildren } from "react";
 import ReactQuerySetting from "src/libraries/reactQuery/ReactQuerySetting";
 import RecoilSetting from "src/libraries/recoil/RecoilSetting";
 import ToastSetting from "src/libraries/toast/ToastSetting";
+import { GlobalStyle } from "src/styles";
 
 export default function AppRegister({ children }: PropsWithChildren) {
   return (
     <RecoilSetting>
       <ReactQuerySetting>
+        <GlobalStyle />
         {children}
         <ToastSetting />
       </ReactQuerySetting>
