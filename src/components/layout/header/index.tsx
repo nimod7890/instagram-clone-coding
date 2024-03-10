90a462c4cfab7aff5b7ffce18e9004b9b6b37e7d
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 16:14:28 +0900

    feat: header 레이아웃 구축

---
+++ src/components/layout/header/index.tsx
@@ -0,0 +1,48 @@
+import { GlobalSize } from "src/constants";
+import { useWindowSize } from "src/hooks/@common";
+import { theme } from "src/styles";
+import styled from "styled-components";
+
+export default function Header() {
+  const { width } = useWindowSize();
+  const isMobileSize = width ? width <= 1000 : false;
+
+  return (
+    <OuterContainer>
+      <InnterContainer>
+        <div>logo</div>
+        <div>search bar</div>
+        {isMobileSize ? null : <div>menu bar</div>}
+      </InnterContainer>
+    </OuterContainer>
+  );
+}
+
+/** styles */
+
+const OuterContainer = styled.div`
+  top: 0;
+
+  width: 100%;
+  height: ${GlobalSize.HeaderHeight};
+  min-height: ${GlobalSize.HeaderHeight};
+
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  background-color: ${theme.palette.white};
+  border-bottom: 1px solid ${theme.palette.gray200};
+`;
+
+const InnterContainer = styled.div`
+  width: calc(100% - 40px);
+  max-width: calc(${GlobalSize.WindowWidthMax} - 40px);
+
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  gap: 10px;
+`;
