89773e632f1dcc6d68fd2cad85653b98cb2b889f
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 14:32:02 +0900

    feat: create window size hook

---
+++ src/hooks/@common/useWindowSize.ts
@@ -0,0 +1,32 @@
+// https://9yujin.tistory.com/20
+import { debounce } from "lodash";
+import { useEffect, useState } from "react";
+
+type WindowSize = {
+  width: number | undefined;
+  height: number | undefined;
+};
+
+export default function useWindowSize() {
+  const [windowSize, setWindowSize] = useState<WindowSize>({
+    width: undefined,
+    height: undefined,
+  });
+
+  useEffect(() => {
+    const handleResize = debounce(() => {
+      if (window === undefined) {
+        const { innerWidth: width, innerHeight: height } = window;
+        setWindowSize({ width, height });
+      }
+    }, 200);
+
+    window.addEventListener("resize", handleResize);
+
+    handleResize();
+
+    return () => window.removeEventListener("resize", handleResize);
+  }, []);
+
+  return windowSize;
+}
