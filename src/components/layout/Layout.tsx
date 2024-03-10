6c50925c99a3b1dc1b34bfaf6aad2113c748848c
nimod7890 <nimod1234@g.skku.edu>
2024-03-10 15:47:35 +0900

    design: update layout container css

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -1,8 +1,21 @@
 import AuthenticatedLayout from "src/components/layout/AuthenticatedLayout";
 import UnAuthenticatedLayout from "src/components/layout/UnAuthenticatedLayout";
+import { GlobalSize } from "src/constants";
+import styled from "styled-components";
 
 export default function Layout() {
   const isAuthenticated: boolean = true;
 
-  return isAuthenticated ? <UnAuthenticatedLayout /> : <AuthenticatedLayout />;
+  return (
+    <Container>
+      {isAuthenticated ? <AuthenticatedLayout /> : <UnAuthenticatedLayout />}
+    </Container>
+  );
 }
+
+const Container = styled.div`
+  min-width: ${GlobalSize.WindowWidthMin};
+  min-height: 100vh;
+
+  overflow: hidden;
+`;
