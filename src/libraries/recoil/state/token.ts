9876ab2846ac4e322b473b80b5178a046118b209
nimod7890 <nimod1234@g.skku.edu>
2024-03-11 14:33:52 +0900

    feat: create token, recoil storage hook

--- src/libraries/recoil/state/token.ts
+++ src/libraries/recoil/state/token.ts
@@ -2,7 +2,7 @@ import { atom } from "recoil";
 import { localStorageEffect } from "src/libraries/recoil";
 import { StorageKeys } from "src/utils/storage";
 
-export const tokenState = atom<string>({
+export const tokenState = atom<string | null>({
   key: StorageKeys.Token,
   default: "",
   effects: [localStorageEffect(StorageKeys.Token)],
