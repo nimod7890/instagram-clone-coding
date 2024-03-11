8447a71cd0c81e855d3a925496609e4e0937e1a1
nimod7890 <nimod1234@g.skku.edu>
2024-03-11 14:34:22 +0900

    feat: 로그아웃 시 token, recoil storage clear

--- src/pages/SignOutNavPage.tsx
+++ src/pages/SignOutNavPage.tsx
@@ -1,7 +1,16 @@
+import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
+import { useAppRepository, useAuthStorage } from "src/hooks/@common";
 import RoutePath from "src/routes/routePath";
 
 export default function SignOutNavPage() {
-  // Todo: sign out 처리
+  const { clear: appRepositoryClear } = useAppRepository();
+  const { clear: authStorageClear } = useAuthStorage();
+
+  useEffect(() => {
+    appRepositoryClear();
+    authStorageClear();
+  }, []);
+
   return <Navigate to={RoutePath.Signin} replace />;
 }
