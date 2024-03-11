5f094fa22d519345d4d71b0d102a9687a72214ee
nimod7890 <nimod1234@g.skku.edu>
2024-03-11 18:05:36 +0900

    feat: axios interceptors에 토큰 재발급 로직 추가

--- src/apis/index.ts
+++ src/apis/index.ts
@@ -1,6 +1,7 @@
 import axios, { AxiosInstance } from "axios";
 import * as jsonwebtoken from "jsonwebtoken";
 import { JwtPayload } from "jsonwebtoken";
+import { useAuthStorage } from "src/hooks/@common";
 import lStorage, { StorageKeys } from "src/utils/storage";
 
 const apiClient: AxiosInstance = axios.create({
@@ -14,21 +15,31 @@ const apiClient: AxiosInstance = axios.create({
 });
 
 apiClient.interceptors.request.use(
-  (config) => {
-    const jwt = lStorage.get(StorageKeys.Token) ?? "";
-    const decodedJwt: JwtPayload = jsonwebtoken.decode(jwt) as JwtPayload;
+  async (config) => {
+    const jwt = lStorage.get(StorageKeys.Token);
+    const { exp: expiredDate } = jsonwebtoken.decode(jwt) as JwtPayload;
     const currentTime = new Date().getTime() / 1000;
 
-    if (decodedJwt.exp ?? 0 < currentTime) {
-      // Todo: 서버에 토큰 재발급 요청 코드 작성 필요
-      console.error("서버에 토큰 재발급 요청");
+    /** token 재발급 */
+    if (expiredDate && expiredDate < currentTime) {
+      console.info("token 재발급 시도", expiredDate, currentTime);
+
+      const { token, setAuthData } = useAuthStorage();
+
+      const { jwt } = (await axios.post("/auth/jwt", { jwt: token })).data;
+
+      console.info("token 재발급 완료", jwt);
+
+      setAuthData(jwt);
+
+      // if (config.headers) {
+      //   config.headers.Authorization = `Bearer ${jwt}`;
+      // }
     }
+
     return config;
   },
-  (error) => {
-    console.error(error);
-    return Promise.reject(error);
-  }
+  (error) => Promise.reject(error)
 );
 
 apiClient.interceptors.response.use(
