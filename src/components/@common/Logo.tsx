c9dcd16afee007d3ea510936dd02e7ba64e956b7
nimod7890 <nimod1234@g.skku.edu>
2024-03-11 23:12:58 +0900

    refactor: logo component

--- src/components/@common/Logo.tsx
+++ src/components/@common/Logo.tsx
@@ -1,7 +1,7 @@
 import { ReactComponent as LogoSvg } from "src/assets/logo.svg";
 
 export default function Logo({
-  width = "140",
+  width = 140,
   ...props
 }: React.SVGProps<SVGSVGElement>) {
   return <LogoSvg width={width} {...props} />;
