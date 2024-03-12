20bbdc095f6ddbd55b31eb6cc3931b7bcf89a91f
nimod7890 <nimod1234@g.skku.edu>
2024-03-12 20:57:18 +0900

    feat: 회원가입 hook 추가

---
+++ src/hooks/mutation/auth/useSignup.ts
@@ -0,0 +1,41 @@
+import { useMutation } from "@tanstack/react-query";
+import { padStart } from "lodash";
+import { useNavigate } from "react-router-dom";
+import apiClient from "src/apis";
+import { SignupFormInput } from "src/pages/SignupPage";
+import RoutePath from "src/routes/routePath";
+import { DateSelectFormType } from "src/types";
+
+export default function useSignup() {
+  const navigate = useNavigate();
+
+  const { mutate: signin, ...rest } = useMutation({
+    mutationFn: async ({
+      birthDate,
+      loginId,
+      password,
+      ...rest
+    }: SignupFormInput) => {
+      return (
+        await apiClient.post(`/auth/sign-up`, {
+          loginId: loginId.trim(),
+          password: password.trim(),
+          birthDate: getFormattedDate(birthDate),
+          ...rest,
+        })
+      ).data.result;
+    },
+    onSuccess: () => navigate(RoutePath.Signin),
+  });
+
+  return { signin, ...rest };
+}
+
+/** utils */
+function getFormattedDate({ year, month, day }: DateSelectFormType): string {
+  const formattedYear = padStart(year.toString(), 4, "0");
+  const formattedMonth = padStart(month.toString(), 2, "0");
+  const formattedDay = padStart(day.toString(), 2, "0");
+
+  return `${formattedYear}-${formattedMonth}-${formattedDay}`;
+}
