2660dedba011a7563b8ebad6e0f099d2d09f542f
nimod7890 <nimod1234@g.skku.edu>
2024-03-12 14:03:11 +0900

    design: add label

--- src/components/@common/Checkbox.tsx
+++ src/components/@common/Checkbox.tsx
@@ -1,26 +1,66 @@
-import { Icon } from "src/components/@common";
+import { ReactNode } from "react";
+import { Link } from "react-router-dom";
+import { Icon, Typography } from "src/components/@common";
 import { theme } from "src/styles";
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 
 type CheckboxProps = {
-  checked: boolean;
-} & Pick<React.InputHTMLAttributes<HTMLInputElement>, "onClick">;
+  label: ReactNode | string;
+  link?: ReactNode | string;
+  containerStyles?: ReturnType<typeof css>;
+} & React.InputHTMLAttributes<HTMLInputElement>;
 
 export default function Checkbox({
   checked,
+  label,
+  link,
   onClick,
+  containerStyles,
   ...props
 }: CheckboxProps) {
   return (
-    <StyledCheckbox checked={checked} onClick={onClick} {...props}>
-      {checked ? (
-        <Icon icon="check" color={checked ? "primary500" : "border"} />
-      ) : null}
-    </StyledCheckbox>
+    <Container css={containerStyles}>
+      <LabelContainer>
+        <Typography>{label}</Typography>
+        {link && <StyledLink to="#">{link}</StyledLink>}
+      </LabelContainer>
+      <StyledCheckbox checked={checked} onClick={onClick} readOnly {...props}>
+        {checked ? (
+          <Icon
+            icon="check"
+            color={checked ? "primary500" : "border"}
+            size={"14px"}
+          />
+        ) : null}
+      </StyledCheckbox>
+    </Container>
   );
 }
 
-const StyledCheckbox = styled.div<{ checked: boolean }>`
+/** styles */
+
+const Container = styled.div<{ css?: ReturnType<typeof css> }>`
+  width: 100%;
+  height: 50px;
+
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+
+  gap: 12px;
+
+  ${({ css }) => css}
+`;
+
+const LabelContainer = styled.div`
+  width: 100%;
+
+  display: flex;
+  flex-direction: column;
+`;
+
+const StyledCheckbox = styled.div<{ checked?: boolean | undefined }>`
+  transition: 0.05s all;
   display: flex;
   align-items: center;
   justify-content: center;
@@ -29,12 +69,18 @@ const StyledCheckbox = styled.div<{ checked: boolean }>`
   height: 20px;
 
   border: 1px solid
-    ${({ checked }) =>
+    ${({ checked = false }) =>
       checked ? theme.palette.primary500 : theme.palette.border};
   border-radius: 6px;
 
   background-color: ${({ checked }) =>
-    checked ? theme.palette.primary200 : theme.palette.white};
+    checked ? "#EFF8FF" : theme.palette.white};
 
   cursor: pointer;
+`;
+
+const StyledLink = styled(Link)`
+  ${theme.typography.body1Regular}
+
+  color: ${theme.palette.primary500};
 `;
