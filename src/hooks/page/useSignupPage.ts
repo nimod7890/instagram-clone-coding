f15d066af439f66015cdcd302e5577b4261de2d4
nimod7890 <nimod1234@g.skku.edu>
2024-03-12 21:02:13 +0900

    refactor: 회원가입 페이지 훅

---
+++ src/hooks/page/useSignupPage.ts
@@ -0,0 +1,54 @@
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import {
+  BasicInfoStep,
+  BirthdayStep,
+  TermsAgreementStep,
+} from "src/components/auth/signup";
+import { useSignup } from "src/hooks/mutation";
+import { DateSelectFormType } from "src/types";
+
+const Steps = [BasicInfoStep, BirthdayStep, TermsAgreementStep];
+
+export type SignupFormInput = {
+  loginId: string;
+  password: string;
+  realName: string;
+  phone: string;
+  birthDate: DateSelectFormType;
+};
+
+export default function useSignupPage() {
+  const [step, setStep] = useState<number>(0);
+
+  const forms = useForm<SignupFormInput>({
+    mode: "onChange",
+    defaultValues: {
+      loginId: "",
+      password: "",
+      realName: "",
+      phone: "",
+      birthDate: { day: 0, month: 0, year: 0 },
+    },
+  });
+
+  const { handleSubmit } = forms;
+  const { signin } = useSignup();
+
+  const onSubmit = async (data: SignupFormInput) => {
+    signin(data);
+  };
+
+  const CurrentStep = Steps[step];
+
+  const handlePrevStep = () => setStep((prevStep) => prevStep - 1);
+  const handleNextStep = () => setStep((prevStep) => prevStep + 1);
+
+  return {
+    forms,
+    CurrentStep,
+    handlePrevStep: step === 0 ? undefined : handlePrevStep,
+    handleNextStep: step === 2 ? undefined : handleNextStep,
+    handleSubmit: step === 2 ? handleSubmit(onSubmit) : undefined,
+  };
+}
