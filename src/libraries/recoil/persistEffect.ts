a98739f954305056abadbc7ea45face975ce8cdc
nimod7890 <nimod1234@g.skku.edu>
2024-03-13 10:55:43 +0900

    feat: value null 일 경우 remove local storage key

--- src/libraries/recoil/persistEffect.ts
+++ src/libraries/recoil/persistEffect.ts
@@ -4,10 +4,14 @@ export const localStorageEffect =
   (key: string) =>
   ({ setSelf, onSet }: any) => {
     const savedValue = lStorage.get(key);
+
     if (savedValue !== null) {
       setSelf(savedValue);
     }
+
     onSet((newValue: any, _: any, isReset: boolean) => {
-      isReset ? lStorage.remove(key) : lStorage.set(key, newValue);
+      isReset || newValue === null
+        ? lStorage.remove(key)
+        : lStorage.set(key, newValue);
     });
   };
