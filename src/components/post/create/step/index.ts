05a344d8336f112a1baaa0cdde5810cf626f469b
nimod7890 <nimod1234@g.skku.edu>
2024-03-15 00:18:47 +0900

    refactor: use barrel

---
+++ src/components/post/create/step/index.ts
@@ -0,0 +1,3 @@
+export { default as ImageInputStep } from "./ImageInputStep";
+export { default as ImagePreviewStep } from "./ImagePreviewStep";
+export { default as WriteTextStep } from "./WriteTextStep";
