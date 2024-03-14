98eebc69b6b0bc67ba11facce075422cc9605845
nimod7890 <nimod1234@g.skku.edu>
2024-03-15 00:47:32 +0900

    refactor: add create post modal hook

--- src/components/post/create/CreatePostModal.tsx
+++ src/components/post/create/CreatePostModal.tsx
@@ -1,77 +1,29 @@
-import { useState } from "react";
-import { Modal, ModalProps, Icon } from "src/components/@common";
-import {
-  ImageInputStep,
-  ImagePreviewStep,
-  WriteTextStep,
-} from "src/components/post/create/step";
-import { useFunnel, useWindowSize } from "src/hooks/@common";
-import { UploadImageFileType } from "src/types";
-
-const enum CreatePostStep {
-  InputImage,
-  PreviewImage,
-  WriteText,
-}
+import { ImageInputStep, ImagePreviewStep, WriteTextStep } from "./step";
+import { Modal, ModalProps } from "src/components/@common";
+import { useCreatePostModal } from "src/hooks/modal";
+import { CreatePostStep } from "src/hooks/modal/useCreatePostModal";
 
 export default function CreatePostModal({
   isOpen,
   close,
 }: Pick<ModalProps, "isOpen" | "close">) {
-  const { step, handleNextStep, handlePrevStep } = useFunnel();
+  const {
+    post,
+    step,
+    headerOptions,
+    modalStyles,
+    handleImagesInput,
+    handleInputText,
+  } = useCreatePostModal();
 
-  const [feedText, setFeedText] = useState<string>("");
-  const [images, setImages] = useState<UploadImageFileType[]>([]);
-
-  const handleSubmit = () => {};
-
-  const handleImagesInput = (images: UploadImageFileType[]) => {
-    setImages(images);
-    handleNextStep();
-  };
-
-  const { width } = useWindowSize();
-  const height =
-    width && width < 656
-      ? step === CreatePostStep.WriteText
-        ? `${width + 300}px`
-        : `${width}px`
-      : "716px";
-
-  const headerOptions = (() => {
-    let options = { title: "새 게시물 만들기" };
-
-    if (step !== CreatePostStep.InputImage) {
-      options = Object.assign(options, {
-        left: { onClick: handlePrevStep, children: <Icon icon="arrow-left" /> },
-      });
-    }
-
-    switch (step) {
-      case CreatePostStep.InputImage:
-        return options;
-      case CreatePostStep.PreviewImage:
-        return {
-          ...options,
-          right: { onClick: handleNextStep, children: "다음" },
-        };
-      case CreatePostStep.WriteText:
-        return {
-          ...options,
-          right: { onClick: handleSubmit, children: "공유" },
-        };
-    }
-  })();
+  const { images } = post;
 
   return (
     <Modal
       isOpen={isOpen}
       close={close}
       header={headerOptions}
-      styles={{
-        width: step === CreatePostStep.WriteText ? "956px" : "656px",
-        height,
-      }}
+      styles={modalStyles}
     >
       {(() => {
         switch (step) {
@@ -80,13 +32,7 @@ export default function CreatePostModal({
           case CreatePostStep.PreviewImage:
             return <ImagePreviewStep images={images} />;
           case CreatePostStep.WriteText:
-            return (
-              <WriteTextStep
-                feedText={feedText}
-                images={images}
-                onInputText={setFeedText}
-              />
-            );
+            return <WriteTextStep post={post} onInputText={handleInputText} />;
         }
       })()}
     </Modal>
---
+++ src/hooks/modal/index.ts
@@ -0,0 +1,1 @@
+export { default as useCreatePostModal } from "./useCreatePostModal";
