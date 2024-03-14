6102f4ce2cac5b3e4a7b74cd5543eebaa0a22bc8
nimod7890 <nimod1234@g.skku.edu>
2024-03-14 19:58:49 +0900

    feat: 이미지 생성 기능 화면 구현

--- src/components/@common/AppMenu.tsx
+++ src/components/@common/AppMenu.tsx
@@ -1,13 +1,18 @@
 import { NavIconButton, IconButton } from "src/components/@common";
+import CreatePostModal from "src/components/post/create/CreatePostModal";
+import { useModal } from "src/hooks/@common";
 import RoutePath from "src/routes/routePath";
 
 export default function AppMenu() {
+  const { isOpen, open, close } = useModal();
+
   return (
     <>
       <NavIconButton icon="home" to={RoutePath.Home} />
       <NavIconButton icon="send" to={RoutePath.DirectMessage} />
-      <IconButton icon="plus-square" />
+      <IconButton icon="plus-square" onClick={open} />
       <NavIconButton icon="heart" />
+      <CreatePostModal isOpen={isOpen} close={close} />
     </>
   );
 }
---
+++ src/components/post/create/CreatePostModal.tsx
@@ -0,0 +1,93 @@
+import ImageInputStep from "./step/ImageInputStep";
+import { useState } from "react";
+import { Modal, Icon } from "src/components/@common";
+import { ModalProps } from "src/components/@common/modal";
+import ImagePreviewStep from "src/components/post/create/step/ImagePreviewStep";
+import WriteTextStep from "src/components/post/create/step/WriteTextStep";
+import { useFunnel, useWindowSize } from "src/hooks/@common";
+
+export type ImageFileType = { imageUrl: string; file: File };
+
+const enum CreatePostStep {
+  InputImage,
+  PreviewImage,
+  WriteText,
+}
+export default function CreatePostModal({
+  isOpen,
+  close,
+}: Pick<ModalProps, "isOpen" | "close">) {
+  const { step, handleNextStep, handlePrevStep } = useFunnel();
+
+  const [feedText, setFeedText] = useState<string>("");
+  const [images, setImages] = useState<ImageFileType[]>([]);
+
+  const handleSubmit = () => {};
+
+  const handleImagesInput = (images: ImageFileType[]) => {
+    setImages(images);
+    handleNextStep();
+  };
+
+  const { width } = useWindowSize();
+  const height =
+    width && width < 656
+      ? step === CreatePostStep.WriteText
+        ? `${width + 300}px`
+        : `${width}px`
+      : "716px";
+
+  const headerOptions = (() => {
+    let options = { title: "새 게시물 만들기" };
+
+    if (step !== CreatePostStep.InputImage) {
+      options = Object.assign(options, {
+        left: { onClick: handlePrevStep, children: <Icon icon="arrow-left" /> },
+      });
+    }
+
+    switch (step) {
+      case CreatePostStep.InputImage:
+        return options;
+      case CreatePostStep.PreviewImage:
+        return {
+          ...options,
+          right: { onClick: handleNextStep, children: "다음" },
+        };
+      case CreatePostStep.WriteText:
+        return {
+          ...options,
+          right: { onClick: handleSubmit, children: "공유" },
+        };
+    }
+  })();
+
+  return (
+    <Modal
+      isOpen={isOpen}
+      close={close}
+      header={headerOptions}
+      styles={{
+        width: step === CreatePostStep.WriteText ? "956px" : "656px",
+        height,
+      }}
+    >
+      {(() => {
+        switch (step) {
+          case CreatePostStep.InputImage:
+            return <ImageInputStep onImagesInput={handleImagesInput} />;
+          case CreatePostStep.PreviewImage:
+            return <ImagePreviewStep images={images} />;
+          case CreatePostStep.WriteText:
+            return (
+              <WriteTextStep
+                feedText={feedText}
+                images={images}
+                onInputText={setFeedText}
+              />
+            );
+        }
+      })()}
+    </Modal>
+  );
+}
