76602a94e7252b7863c6e7f1785c74cb410bb7b6
nimod7890 <nimod1234@g.skku.edu>
2024-03-15 16:39:15 +0900

    feat: local storage로 user 관리

--- src/libraries/recoil/state/user.ts
+++ src/libraries/recoil/state/user.ts
@@ -1,8 +1,11 @@
+import uuid from "react-uuid";
 import { atom } from "recoil";
-import { RecoilKeys } from "src/libraries/recoil";
+import { RecoilKeys, localStorageEffect } from "src/libraries/recoil";
 import { UserStateType } from "src/types";
+import { StorageKeys } from "src/utils/storage";
 
 export const userState = atom<UserStateType>({
-  key: RecoilKeys.User,
+  key: `${RecoilKeys.User}${uuid()}`,
   default: null,
+  effects: [localStorageEffect(StorageKeys.User)],
 });
