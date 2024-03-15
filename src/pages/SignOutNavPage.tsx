a3675583418b66fd2a291c18028883539eea30ca
nimod7890 <nimod1234@g.skku.edu>
2024-03-15 16:39:49 +0900

    fix: bad useState

--- src/pages/SignOutNavPage.tsx
+++ src/pages/SignOutNavPage.tsx
@@ -1,10 +1,14 @@
+import { useEffect } from "react";
 import { Navigate } from "react-router-dom";
 import { useAppRepository } from "src/hooks/@common";
 import RoutePath from "src/routes/routePath";
 
 export default function SignOutNavPage() {
   const { clear } = useAppRepository();
-  clear();
+
+  useEffect(() => {
+    clear();
+  }, []);
 
   return <Navigate to={RoutePath.Signin} replace />;
 }
