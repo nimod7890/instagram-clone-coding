48dc6432259e4ec065a5fb9167ba34933c85bfd2
nimod7890 <nimod1234@g.skku.edu>
2024-03-17 19:51:39 +0900

    feat: 좋아요 리스트 내 사용자 컴포넌트 클릭 시 유저 페이지로 이동

--- src/components/post/details/LikesModal.tsx
+++ src/components/post/details/LikesModal.tsx
@@ -1,7 +1,10 @@
+import { useNavigate } from "react-router-dom";
 import { Modal, ModalProps } from "src/components/@common";
 import { PostProfile } from "src/components/post";
-import { useAppRepository } from "src/hooks/@common";
+import { getUserPagePath } from "src/routes/routePath";
+import { theme } from "src/styles";
 import { UserType } from "src/types";
+import styled from "styled-components";
 
 type LikesModalProps = { participants: UserType[] } & ModalProps;
 
@@ -10,13 +13,33 @@ export default function LikesModal({
   isOpen,
   close,
 }: LikesModalProps) {
-  const { userData } = useAppRepository();
+  const navigate = useNavigate();
+
   return (
     <Modal isOpen={isOpen} close={close}>
-      <PostProfile loginId={userData?.loginId} />
+      <Divider />
       {participants.map(({ id, loginId }) => (
-        <PostProfile key={id} loginId={loginId} />
+        <>
+          <NavigateProfileButton
+            key={id}
+            onClick={() => navigate(getUserPagePath(loginId))}
+          >
+            <PostProfile loginId={loginId} />
+          </NavigateProfileButton>
+          <Divider key={id} />
+        </>
       ))}
     </Modal>
   );
 }
+
+const Divider = styled.hr`
+  width: 100%;
+  padding: 0;
+  margin: 0;
+  border-bottom: 1px solid ${theme.palette.gray300};
+`;
+
+const NavigateProfileButton = styled.button`
+  width: 100%;
+`;
