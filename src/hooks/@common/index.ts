ff629766a7e9db05d9f2be21917f3f01e4901e2a
nimod7890 <nimod1234@g.skku.edu>
2024-03-17 20:21:15 +0900

    add: export useToggle using useModal

--- src/hooks/@common/index.ts
+++ src/hooks/@common/index.ts
@@ -1,5 +1,8 @@
 export { default as useWindowSize } from "./useWindowSize";
-export { default as useModal } from "./useModal";
+
+export { default as useToggle } from "./useToggle";
+export { default as useModal } from "./useToggle";
+
 export { default as useFunnel } from "./useFunnel";
 export { default as useAppRepository } from "./useAppRepository";
 export { default as useAuthStorage } from "./useAuthStorage";
