28b1225a5c69b0033b1f170bfd5b2252dfdc2dc2
nimod7890 <nimod1234@g.skku.edu>
2024-03-17 19:46:00 +0900

    rename: like list button & modal

--- src/components/post/details/LikesButton.tsx
+++ src/components/post/details/LikesButton.tsx
@@ -1,24 +1,29 @@
+import LikesModal from "./LikesModal";
 import React from "react";
+import { useModal } from "src/hooks/@common";
 import { useGetLikes } from "src/hooks/query";
 import { theme } from "src/styles";
 import styled from "styled-components";
 
-type LikeListButtonProps = { postId: number } & Omit<
+type LikesButtonProps = { postId: number } & Omit<
   React.ButtonHTMLAttributes<HTMLButtonElement>,
   "children" | "onClick"
 >;
 
-export default function LikeListButton({
-  postId,
-  ...props
-}: LikeListButtonProps) {
+export default function LikesButton({ postId, ...props }: LikesButtonProps) {
+  const { isOpen, open, close } = useModal();
   const { likes, isPending } = useGetLikes(postId);
 
   return (
     <>
-      <Button {...props} disabled={isPending}>
+      <Button {...props} disabled={isPending} onClick={open}>
         좋아요 {likes?.totalCount}개
       </Button>
+      <LikesModal
+        participants={likes.feedLikeList}
+        isOpen={isOpen}
+        close={close}
+      />
     </>
   );
 }
