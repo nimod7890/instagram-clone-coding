07ee73a0247a4aaa5957a6484a575b2cd9cbcbb7
nimod7890 <nimod1234@g.skku.edu>
2024-03-17 19:33:18 +0900

    move: post details

--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -2,4 +2,4 @@ export { default as PostProfile } from "./PostProfile";
 
 export { default as ArrowLeftIcon } from "./create/step/ArrowLeftIcon";
 
-export { default as Feed } from "./feed/Feed";
+export { default as Feed } from "./feed";
