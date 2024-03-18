693b0706049782c71d11f35d981f2951d2e66673
nimod7890 <nimod1234@g.skku.edu>
2024-03-18 12:05:25 +0900

    feat: 내 페이지인 경우에만 tab 보이기

--- src/components/user/tab/index.tsx
+++ src/components/user/tab/index.tsx
@@ -1,5 +1,6 @@
 import UserNavTabButton from "./UserNavTabButton";
 import { Outlet } from "react-router-dom";
+import { useAppRepository } from "src/hooks/@common";
 import {
   getUserBookMarkPagePath,
   getUserLikePagePath,
@@ -10,6 +11,9 @@ import styled from "styled-components";
 type UserActivityTabsProps = { loginId: string };
 
 export default function UserActivityTabs({ loginId }: UserActivityTabsProps) {
+  const { userData } = useAppRepository();
+  const myPage = userData?.loginId === loginId;
+
   const BUTTONS: { label: string; link: string }[] = [
     { label: "게시글", link: getUserPagePath(loginId) },
     { label: "좋아요", link: getUserLikePagePath(loginId) },
@@ -19,11 +23,13 @@ export default function UserActivityTabs({ loginId }: UserActivityTabsProps) {
   return (
     <>
       <Container>
-        {BUTTONS.map(({ label, link }) => (
-          <UserNavTabButton key={label} to={link}>
-            {label}
-          </UserNavTabButton>
-        ))}
+        {myPage
+          ? BUTTONS.map(({ label, link }) => (
+              <UserNavTabButton key={label} to={link}>
+                {label}
+              </UserNavTabButton>
+            ))
+          : null}
       </Container>
       <Body>
         <Outlet />
