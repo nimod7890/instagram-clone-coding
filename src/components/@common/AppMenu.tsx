747ffd5e57928626356396cc3f8ba2183806034f
nimod7890 <nimod1234@g.skku.edu>
2024-03-18 12:20:37 +0900

    feat: 좋아요, 북마크 페이지 라우팅

--- src/components/@common/AppMenu.tsx
+++ src/components/@common/AppMenu.tsx
@@ -1,9 +1,15 @@
 import { NavIconButton, IconButton } from "src/components/@common";
 import { CreatePostModal } from "src/components/modal";
-import { useModal } from "src/hooks/@common";
-import RoutePath from "src/routes/routePath";
+import { useAppRepository, useModal } from "src/hooks/@common";
+import RoutePath, { getUserLikePagePath } from "src/routes/routePath";
 
 export default function AppMenu() {
+  const { userData } = useAppRepository();
+
+  if (!userData) {
+    return null;
+  }
+
   const { isOpen, open, close } = useModal();
 
   return (
@@ -11,7 +17,7 @@ export default function AppMenu() {
       <NavIconButton icon="home" to={RoutePath.Home} />
       <NavIconButton icon="send" to={RoutePath.DirectMessage} />
       <IconButton icon="plus-square" onClick={open} />
-      <NavIconButton icon="heart" />
+      <NavIconButton icon="heart" to={getUserLikePagePath(userData.loginId)} />
       <CreatePostModal isOpen={isOpen} close={close} />
     </>
   );
