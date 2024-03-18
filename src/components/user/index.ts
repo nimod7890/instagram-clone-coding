70cf7924d482836b4a949ae9a37a161b7e9ceac5
nimod7890 <nimod1234@g.skku.edu>
2024-03-18 13:13:35 +0900

    refactor: export barrel

--- src/components/user/index.ts
+++ src/components/user/index.ts
@@ -1,3 +1,4 @@
 export { default as Profile } from "./profile";
 export { default as UserActivityTabs } from "./tab";
 export { default as GridPosts } from "./tab/GridPosts";
+export { default as PageLoading } from "./PageLoading";
