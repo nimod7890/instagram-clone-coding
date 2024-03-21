2dacb113b8c844b11bd500b33689622c753d5713
nimod7890 <nimod1234@g.skku.edu>
2024-03-21 11:08:57 +0900

    move: feed files

--- src/components/post/index.ts
+++ src/components/post/index.ts
@@ -1,7 +1,5 @@
 export { default as PostProfile } from "./PostProfile";
 
-export { default as Feed } from "./feed";
-
 export { default as PostActionsBar } from "./details/PostActionsBar";
 export { default as LikesButton } from "./details/LikesButton";
 export { default as PostCreatedAt } from "./details/PostCreatedAt";
